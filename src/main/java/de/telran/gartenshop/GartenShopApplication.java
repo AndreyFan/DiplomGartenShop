e5283b3ca0a7e40fd15e3c26773d3c9971a392b9
AndreyFan <fesha1976@gmail.com>
2025-01-24 11:43:22 +0100

    second commit

---
+++ src/main/java/de/telran/gartenshop/GartenShopApplication.java
@@ -0,0 +1,13 @@
+package de.telran.gartenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GartenShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GartenShopApplication.class, args);
+	}
+
+}
