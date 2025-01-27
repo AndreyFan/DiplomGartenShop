1d5b9e10228b1dfaf326e2f3905bd8dd19a68f57
NatLi-zp <natali.gordienko@gmail.com>
2025-01-27 15:46:54 +0100

    task2_ng: add enums Role, OrderStatus ; add classes CategoryDto, CategoryEntity, ProductDto, ProductEntity

---
+++ src/main/java/de/telran/gartenshop/dto/requestDto/CategoryRequestDto.java
@@ -0,0 +1,14 @@
+package de.telran.gartenshop.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryRequestDto {
+    private String name;
+}
