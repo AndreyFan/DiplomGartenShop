5a1f65690e149540eb4fcdde2f51b6200f0290ba
AndreyFan <fesha1976@gmail.com>
2025-01-28 17:05:57 +0100

    added methods to UserController

--- src/main/java/de/telran/gartenshop/controller/UserController.java
+++ src/main/java/de/telran/gartenshop/controller/UserController.java
@@ -1,15 +1,47 @@
 package de.telran.gartenshop.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.telran.gartenshop.dto.requestDto.UserRequestDto;
+import de.telran.gartenshop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public String getTest(){
+    public String getTest() {
         return "Test Task_2";
     }
+
+
+    @PostMapping("/register")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void registerUser(@RequestBody UserRequestDto userRequestDto) {
+        userService.registerUser(userRequestDto);
+    }
+
+    @PutMapping("/{userId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void updateUser(@RequestBody UserRequestDto userRequestDto){
+        userService.updateUser(userRequestDto);
+    }
+
+    @DeleteMapping("/{userId}")
+@ResponseStatus(value = HttpStatus.OK)
+    public void deleteUser(@PathVariable Long userId){
+        userService.deleteUser(userId);
+    }
+
+
 }
---
+++ src/main/java/de/telran/gartenshop/service/UserService.java
@@ -0,0 +1,31 @@
+package de.telran.gartenshop.service;
+
+import de.telran.gartenshop.dto.requestDto.UserRequestDto;
+import de.telran.gartenshop.mapper.Mappers;
+import de.telran.gartenshop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private UserRepository userRepository;
+    private Mappers mappers;
+
+    @Autowired
+    public UserService(UserRepository userRepository, Mappers mappers) {
+        this.userRepository = userRepository;
+        this.mappers = mappers;
+    }
+
+    public void registerUser(UserRequestDto userRequestDto) {
+    }
+
+    public void updateUser(UserRequestDto userRequestDto) {
+    }
+
+    public void deleteUser(Long userId) {
+    }
+}
