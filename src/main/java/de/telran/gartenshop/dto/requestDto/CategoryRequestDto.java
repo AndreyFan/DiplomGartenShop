dcf7aa2c7b5e241123eea146dbb871cfd8f3e2b9
NatLi-zp <natali.gordienko@gmail.com>
2025-01-31 10:25:40 +0100

    task1.4_ng: DELETE в CategoryController, геттер-сеттер-конструкторы в CategoryRequestDto

--- src/main/java/de/telran/gartenshop/dto/requestDto/CategoryRequestDto.java
+++ src/main/java/de/telran/gartenshop/dto/requestDto/CategoryRequestDto.java
@@ -5,10 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Builder
 public class CategoryRequestDto {
     private String name;
+
+    public CategoryRequestDto() {
+    }
+
+    public CategoryRequestDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
