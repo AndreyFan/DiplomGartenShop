de2f4a2ed512883ef760d5a84dd713f0f141e184
NatLi-zp <natali.gordienko@gmail.com>
2025-02-08 23:07:32 +0100

    task2.9_n Category, Product - комментарии,проверка 08.02.25

--- src/main/java/de/telran/gartenshop/controller/CategoryController.java
+++ src/main/java/de/telran/gartenshop/controller/CategoryController.java
@@ -16,24 +16,28 @@ import java.util.List;
 public class CategoryController {
     private final CategoryService categoryService;
 
+    //Просмотр всех категорий товаров //localhost:8088/categories
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<CategoryResponseDto> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
+    //Добавление новой категории товаров //localhost:8088/categories
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public boolean createCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
         return categoryService.createCategory(categoryRequestDto);
     }
 
+    //Редактирование категории товаров по Id //localhost:8088/categories/3
     @PutMapping(value = "/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
     public CategoryResponseDto updateCategory(@RequestBody CategoryRequestDto categoryRequestDto, @PathVariable Long categoryId) {
         return categoryService.updateCategory(categoryRequestDto, categoryId);
     }
 
+    //Удаление категории товаров по Id //localhost:8088/categories/11
     @DeleteMapping(value = "/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteCategory(@PathVariable Long categoryId) { //delete
