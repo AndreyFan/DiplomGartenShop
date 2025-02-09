78b7df9e1b74d374d0f44bbd26ea6ad294b5a99c
NatLi-zp <natali.gordienko@gmail.com>
2025-02-09 21:28:24 +0100

    task2.11_n Products- изм. Query 'Товар дня', новый put-запрос 'Добавить скидку на товар' 09.02.25

--- src/main/java/de/telran/gartenshop/repository/ProductRepository.java
+++ src/main/java/de/telran/gartenshop/repository/ProductRepository.java
@@ -10,6 +10,12 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>, ProductCustomRepository {
-    @Query(value = "SELECT * FROM Products WHERE DiscountPrice =(SELECT max(DiscountPrice) FROM Products)", nativeQuery = true)
+    @Query(value = "SELECT * FROM Products " +
+            "WHERE (DiscountPrice > 0 AND DiscountPrice IS NOT NULL) " +
+            "AND (Price > 0 AND Price IS NOT NULL) " +
+            "AND (1 - DiscountPrice / Price = " +
+            "(SELECT MAX(1 - DiscountPrice / Price) FROM Products " +
+            "WHERE (DiscountPrice > 0 AND DiscountPrice IS NOT NULL) " +
+            "AND (Price > 0 AND Price IS NOT NULL)))", nativeQuery = true)
     List<ProductEntity> getProductOfDay();
 }
