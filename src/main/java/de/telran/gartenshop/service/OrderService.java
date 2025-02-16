ca777bd3e6cf36bd6685f763e79f30e491d19a96
NatLi-zp <natali.gordienko@gmail.com>
2025-02-16 17:47:07 +0100

    task3.12_n OrderIntegrationTest #4 160225

--- src/main/java/de/telran/gartenshop/service/OrderService.java
+++ src/main/java/de/telran/gartenshop/service/OrderService.java
@@ -105,40 +105,45 @@ public class OrderService {
         }
 
         //2. Заполнение товаров в заказе (преобразование CartItems в OrderItems)
-        Set<CartItemEntity> cartItemEntitySet = userEntity.getCart().getCartItems();
-        Set<OrderItemEntity> orderItemEntitySet = new HashSet<>();
-        for (CartItemEntity orderItem : cartItemEntitySet) {
-            OrderItemEntity createOrderItem = new OrderItemEntity();
-            ProductEntity productEntity = productRepository.findById(orderItem.getProduct().getProductId()).orElse(null);
-            if (productEntity != null) {
-                createOrderItem.setProduct(productEntity);
-                if (productEntity.getDiscountPrice() == null) {
-                    createOrderItem.setPriceAtPurchase(productEntity.getPrice());
+        CartEntity cartEntity = userEntity.getCart();
+        if (cartEntity != null) {
+            Set<CartItemEntity> cartItemEntitySet = userEntity.getCart().getCartItems();
+            Set<OrderItemEntity> orderItemEntitySet = new HashSet<>();
+            for (CartItemEntity orderItem : cartItemEntitySet) {
+                OrderItemEntity createOrderItem = new OrderItemEntity();
+                ProductEntity productEntity = productRepository.findById(orderItem.getProduct().getProductId()).orElse(null);
+                if (productEntity != null) {
+                    createOrderItem.setProduct(productEntity);
+                    if (productEntity.getDiscountPrice() == null) {
+                        createOrderItem.setPriceAtPurchase(productEntity.getPrice());
+                    } else {
+                        createOrderItem.setPriceAtPurchase(productEntity.getDiscountPrice());
+                    }
+                    createOrderItem.setQuantity(orderItem.getQuantity());
+                    createOrderItem.setOrder(createOrderEntity);
+                    orderItemRepository.save(createOrderItem);
+
+                    orderItemEntitySet.add(createOrderItem);
                 } else {
-                    createOrderItem.setPriceAtPurchase(productEntity.getDiscountPrice());
+                    throw new IllegalArgumentException("Product " + orderItem.getProduct().getName() + " not found.");
                 }
-                createOrderItem.setQuantity(orderItem.getQuantity());
-                createOrderItem.setOrder(createOrderEntity);
-                orderItemRepository.save(createOrderItem);
 
-                orderItemEntitySet.add(createOrderItem);
-            } else {
-                throw new IllegalArgumentException("Product " + orderItem.getProduct().getName() + " not found.");
-            }
+                createOrderEntity.setOrderItems(orderItemEntitySet);
+                orderRepository.save(createOrderEntity);
 
-            createOrderEntity.setOrderItems(orderItemEntitySet);
-            orderRepository.save(createOrderEntity);
-
-            //3. Очищение товаров в корзине (удаление CartItems)
-            CartEntity cartEntity = userEntity.getCart();
-            if (cartEntity != null) {
+                //3. Очищение товаров в корзине (удаление CartItems)
+                //  CartEntity cartEntity = userEntity.getCart();
+                // if (cartEntity != null) {
                 Set<CartItemEntity> cartItemSet = cartEntity.getCartItems();
                 for (CartItemEntity item : cartItemSet) {
                     cartItemRepository.delete(item);
                 }
-            } else {
-                throw new IllegalArgumentException("Cart for UserId: " + userId + " not found.");
+//            } else {
+//                throw new IllegalArgumentException("Cart for UserId: " + userId + " not found.");
+//            }
             }
+        } else {
+            throw new IllegalArgumentException("Cart for UserId: " + userId + " not found.");
         }
         return mappers.convertToOrderResponseDto(orderEntity);
     }
