80da70db37ef179662743f708cef15b1d26eaca4
NatLi-zp <natali.gordienko@gmail.com>
2025-02-16 12:19:29 +0100

    task3.12_n CartIntegrationTest 160225

--- src/main/java/de/telran/gartenshop/controller/advice/AdviceController.java
+++ src/main/java/de/telran/gartenshop/controller/advice/AdviceController.java
@@ -10,6 +10,7 @@ import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.FileNotFoundException;
 import java.util.HashMap;
