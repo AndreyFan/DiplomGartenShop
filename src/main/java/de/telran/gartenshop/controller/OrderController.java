56e8781b98df05a5c88142bcc4d892708a16baca
Konstantinisch <ischenkomusik@web.de>
2025-02-23 09:36:59 +0100

    commit on 23.02.25

--- src/main/java/de/telran/gartenshop/controller/OrderController.java
+++ src/main/java/de/telran/gartenshop/controller/OrderController.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/orders")
-public class OrderController {
+public class OrderController implements OrderControllerInterface{
 
     private final OrderService orderService;
 
